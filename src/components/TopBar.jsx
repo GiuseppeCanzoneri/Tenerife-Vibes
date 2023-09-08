32031d86d3047ff03fa03344da4d5e05101dce59
Giuseppe Canzoneri <gcanzoneri6@gmail.com>
2023-09-08 19:31:47 +0200

    new project

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,31 @@
+import { Button, Container, Form, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import logo from "../assets/logo.png";
+
+const TopBar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand>
+          {" "}
+          <img src={logo} alt="logo" height={150} width={150} />{" "}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Escursioni</Nav.Link>
+            <NavDropdown title="Galleria" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Prezzi</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default TopBar;
