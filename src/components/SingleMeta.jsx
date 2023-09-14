3fd77839a43ca90a3965d208376a68c96d3c9949
Giuseppe Canzoneri <gcanzoneri6@gmail.com>
2023-09-14 03:20:37 +0200

    prova

--- src/components/SingleMeta.jsx
+++ src/components/SingleMeta.jsx
@@ -20,7 +20,7 @@ const SingleMeta = ({ meta }) => {
             </Col>
           </Row>
           <Alert variant="info text-dark fs-5 mt-3" style={{ maxWidth: "45%" }}>
-            Prezzo: {meta.prezzo}
+            Prezz: {meta.prezzo}
           </Alert>
         </Card.Body>
       </Card>
